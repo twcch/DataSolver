15337270bf30ae2e22746a6d3ab7069c6cfeb802
謝志謙 <twcch1218@gmail.com>
2024-09-08 21:12:10 +0800

    新增 pandasai util

---
+++ util/ai/pandasai_util.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+from pandasai import Agent
+from pandasai.llm import OpenAI
+
+from util.key_util import KeyUtil
+
+
+class PandasaiUtil():
+
+    def __init__(self):
+        self.__key_util = KeyUtil()
+        self.__api_key = self.__key_util.get_chatgpt_api_key()
+        os.environ["OPENAI_API_KEY"] = self.__api_key
+        self.__llm = OpenAI(api_token=os.environ["OPENAI_API_KEY"])
+
+    def analyze_data_with_chatgpt(self, data: pd.DataFrame, data_description: str, query_message: str) -> pd.DataFrame:
+        agent = Agent(data, description=data_description, config={"llm": self.__llm})
+
+        response = agent.chat(query_message)
+
+        return response
