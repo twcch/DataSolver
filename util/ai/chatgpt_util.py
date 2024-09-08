88ce9451697eda258f42437135e0d81e49cb88c6
謝志謙 <twcch1218@gmail.com>
2024-09-08 20:48:08 +0800

    優化 chatgpt util

--- util/ai/chatgpt_util.py
+++ util/ai/chatgpt_util.py
@@ -4,18 +4,35 @@ from util.path_util import PathUtil
 
 class ChatgptUtil():
 
-    def __init__(self, model="gpt-3.5-turbo"):
+    def __init__(self):
         self.__path_util = PathUtil()
         self.__api_key = self.__get_openai_key()
-        self.__model = model
         self.__client = OpenAI(api_key=self.__api_key)
         self.__chatgpt_role = "你是我的助手，根據問題使用繁體中文回覆"
 
-    def chat_with_chatgpt(self, message):
+    # 使用 chatgpt 3.5 model
+    def chat_with_chatgpt3(self, message):
+        content = self.__chat_with_chatgpt("gpt-3.5-turbo", message)
+
+        return content
+
+    # 使用 chatgpt 4 model
+    def chat_with_chatgpt4(self, message):
+        content = self.__chat_with_chatgpt("gpt-4-turbo", message)
+
+        return content
+
+    def set_model(self, model):
+        self.__model = model
+
+    def set_chatgpt_role(self, chatgpt_role):
+        self.__chatgpt_role = chatgpt_role
+
+    def __chat_with_chatgpt(self, model, message):
         chatgpt_role = self.__chatgpt_role
 
         response = self.__client.chat.completions.create(
-            model=self.__model,
+            model=model,
             messages=[
                 {"role": "assistant", "content": chatgpt_role},
                 {"role": "user", "content": message}
@@ -25,12 +42,6 @@ class ChatgptUtil():
         content = response.choices[0].message.content
 
         return content
-
-    def set_model(self, model):
-        self.__model = model
-
-    def set_chatgpt_role(self, chatgpt_role):
-        self.__chatgpt_role = chatgpt_role
 
     def __get_openai_key(self):
         with open(self.__path_util.get_keys_folder() + "/openai_key.txt", "r") as file:
