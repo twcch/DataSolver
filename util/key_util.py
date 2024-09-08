7e8696900ee235d1ab93d6d2657c2dd271a90a26
謝志謙 <twcch1218@gmail.com>
2024-09-08 21:21:43 +0800

    更新 key util

--- util/key_util.py
+++ util/key_util.py
@@ -1,7 +1,7 @@
 class KeyUtil():
 
     def __init__(self):
-        self.__key_folder = "/Users/twcch/Documents/Code/DataScience/_keys"
+        self.__key_folder = "/Users/twcch/Documents/Code/_keys"
 
     def get_chatgpt_api_key(self):
         file = open(self.__key_folder + "/openai_key.txt", "r")
