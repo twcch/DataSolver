38714ec4839452cef7053821b7fdf7e7a65a2036
謝志謙 <twcch1218@gmail.com>
2024-09-08 22:39:56 +0800

    test

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+# print("Hello World!")
+import yfinance as yf
+import pandas as pd
+import datetime as dt
+
+from core.util.draw.stock_price_trend_chart_util import StockPriceTrendChartUtil
+
+
+stock_price_trend_chart_util = StockPriceTrendChartUtil()
+
+start = dt.datetime(2024, 9, 1)
+end = dt.datetime(2024, 12, 31)
+
+df_2330 = yf.download("2330.TW", start=start, end=end)
+df_2454 = yf.download("2454.TW", start=start, end=end)
+
+print(df_2330)
+
+stock_price_trend_chart_util.plot_stock_price_trend(df_2330.Close, "TSMC", "TSMC", "Date", "Price")
+
+# stock_price_trend_chart_util.plot_two_stock_price_trend(df_2330.Close, "TSMC", df_2454.Close, "MediaTek", "TSMC vs MediaTek", "Date", "Price")
---
+++ test.py
@@ -0,0 +1,6 @@
+from core.util.finance_draw.stock_price_chart_util import StockPriceChartUtil
+
+
+stock_price_chart_util = StockPriceChartUtil("2330.TW")
+
+stock_price_chart_util.plot_stock_close_price_trend_chart()
