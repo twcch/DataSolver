9b5720a4b4a4db4d89cae6e3cfef342654a573ef
謝志謙 <twcch1218@gmail.com>
2024-09-26 21:58:25 +0800

    update

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,12 @@
-from function.project.perfect_trip.post_request.user_data_request import UserDataRequest
+import pandas as pd
 
+data = {
+    "給付項目": "A101",
+    "年度起": 0,
+    "年度末": 4,
+    "金額": 1000
+}
 
-user_data_request = UserDataRequest("http://127.0.0.1:8080/api/users/register")
-user_data_request.insert_data_to_user_master(10000)
+print(f"被保險人於保單年度 {data.get('年度起')} "
+      f"至 {data.get('年度末')} 身故，給付理賠金額 {data.get('金額')}"
+      )
