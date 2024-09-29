a7ce231dc707b5726d9605edb904344e0f113a23
謝志謙 <twcch1218@gmail.com>
2024-09-29 14:44:43 +0800

    update

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,6 @@
-import pandas as pd
+from datetime import datetime
 
-data = {
-    "給付項目": "A101",
-    "年度起": 0,
-    "年度末": 4,
-    "金額": 1000
-}
-
-print(f"被保險人於保單年度 {data.get('年度起')} "
-      f"至 {data.get('年度末')} 身故，給付理賠金額 {data.get('金額')}"
-      )
+now_str = "19931218"
+now = datetime.strptime(now_str, "%Y%m%d")
+now = now.strftime("%Y%m%d")
+print(now)
