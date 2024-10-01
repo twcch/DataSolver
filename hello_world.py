d82c0304643be055dfc1b8662f6e679687e1c681
謝志謙 <twcch1218@gmail.com>
2024-10-01 22:57:27 +0800

    update

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,15 @@
-from datetime import datetime
+import os
+import requests
 
-now_str = "19931218"
-now = datetime.strptime(now_str, "%Y%m%d")
-now = now.strftime("%Y%m%d")
-print(now)
+response = requests.post("https://api.openai.com/v1/chat/completions",
+                         headers={"Content-Type": "application/json",
+                                  "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY")},
+                         json={"model": "gpt-4o-mini",
+                               "messages": [
+                                   {"role": "system", "content": "你是我的翻譯助手，幫我翻譯成英文"},
+                                   {"role": "user", "content": "台灣有幾個縣市"}
+                               ]})
+
+data = response.json()
+content = data["choices"][0]["message"]["content"]
+print(content)
